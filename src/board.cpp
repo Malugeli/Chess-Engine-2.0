161dbdba3de13be5115d5020188b7bea8843d34d
Malugeli <maheralugeli1993@gmail.com>
2026-08-04 19:03:33 +0200

    völlig Verwirrt wie wir do_move machen sollen..

--- src/board.cpp
+++ src/board.cpp
@@ -1,6 +1,35 @@
 #include "board.hpp"
 #include <bit>
 
+void Board::init_bitmaps(){
+  bitmaps[get_index(Color::White, PieceType::Pawn)] = 0xFF00ULL;
+  bitmaps[get_index(Color::White, PieceType::Knight)] = 0x42ULL;
+  bitmaps[get_index(Color::White, PieceType::Bishop)] = 0x24ULL;
+  bitmaps[get_index(Color::White, PieceType::Rook)] = 0x81ULL;
+  bitmaps[get_index(Color::White, PieceType::Queen)] = 0x08ULL;
+  bitmaps[get_index(Color::White, PieceType::King)] = 0x10ULL;
+
+  bitmaps[get_index(Color::Black, PieceType::Pawn)] = 0xFF00ULL << 40;
+  bitmaps[get_index(Color::Black, PieceType::Knight)] = 0x42ULL << 56;
+  bitmaps[get_index(Color::Black, PieceType::Bishop)] = 0x24ULL << 56;
+  bitmaps[get_index(Color::Black, PieceType::Rook)] = 0x81ULL << 56;
+  bitmaps[get_index(Color::Black, PieceType::Queen)] = 0x08ULL << 56;
+  bitmaps[get_index(Color::Black, PieceType::King)] = 0x10ULL << 56;
+}
+
+void Board::init_castling_bitmask(){
+  castling_mask.fill(CastlingRight::Any);
+  castling_mask[static_cast<int>(Square::e1)] &=
+      ~(CastlingRight::White_Short | CastlingRight::White_Long);
+  castling_mask[static_cast<int>(Square::h1)] &= ~CastlingRight::White_Short;
+  castling_mask[static_cast<int>(Square::a1)] &= ~CastlingRight::White_Long;
+
+  castling_mask[static_cast<int>(Square::e8)] &=
+      ~(CastlingRight::Black_Short | CastlingRight::Black_Long);
+  castling_mask[static_cast<int>(Square::h8)] &= ~CastlingRight::Black_Short;
+  castling_mask[static_cast<int>(Square::a8)] &= ~CastlingRight::Black_Long;
+}
+
 uint8_t Board::get_index(Color c, PieceType p) const noexcept {
   assert(p != PieceType::None);
   return static_cast<uint8_t>(( +c << 3 ) | +p); //disjunktes Bitlayout, in den untersten 4 Bits liegt die Color als Bit und 3 Bits für das Piece
@@ -80,43 +109,76 @@ void Board::remove_piece(Color c, PieceType p, Square s) noexcept {
   color_board[+c] &= ~mask;
   mailbox[+s] = Piece::None;
 }
-void Board::make_move(Move m) noexcept {
+
+//bevor ich den Zug ausführe muss den Gamestate speichern um es später wieder rückgängig zu machen
+//statt einem std::stack nutze ich ein festes Array von (vorerst)
+void Board::do_move(Move m, GameState& gamestate) noexcept {
   auto from_square = m.from_sq();
   auto to_square = m.to_sq();
   auto from_piece = mailbox[+from_square];
   auto to_piece = mailbox[+to_square];
 
-  if (m.type_of() == MoveType::Normal) {
+
+  switch(m.type_of()){
+  case MoveType::Normal:
     if (to_piece != Piece::None) {
       remove_piece(color_of(to_piece), piece_of(to_piece), to_square);
     }
     move_piece(color_of(from_piece), piece_of(from_piece), from_square,
                to_square);
+    break;
 
-  }
-  else{
-    assert(false && "Movetypes noch nicht implementiert");
-  }
-  game_state.side_to_move = static_cast<Color>(!+game_state.side_to_move); // so ein genialer Trick um zu switchen omg
+  case MoveType::Castling:
+    switch (to_square) {
+    case Square::h1:
+      move_piece(Color::White, PieceType::Rook, Square::h1, Square::f1);
+      move_piece(Color::White, PieceType::King, Square::e1, Square::g1);
+      break;
+
+    case Square::a1:
+      move_piece(Color::White, PieceType::Rook, Square::a1, Square::d1);
+      move_piece(Color::White, PieceType::King, Square::e1, Square::c1);
+      break;
+
+    case Square::h8:
+      move_piece(Color::Black, PieceType::Rook, Square::h8, Square::f8);
+      move_piece(Color::Black, PieceType::King, Square::e8, Square::g8);
+      break;
+
+    case Square::a8:
+      move_piece(Color::Black, PieceType::Rook, Square::a8, Square::d8);
+      move_piece(Color::Black, PieceType::King, Square::e8, Square::c8);
+      break;
+
+    default:
+      break;
+    }
+     
+    
+  case MoveType::En_Passant:
+  break;
+  
+  case MoveType::Promotion:
+  break;
+}
+  gamestate.side_to_move = static_cast<Color>(!+gamestate.side_to_move); // so ein genialer Trick um zu switchen omg
 };
 
-Board::Board() {
-  bitmaps[get_index(Color::White, PieceType::Pawn)] = 0xFF00ULL;
-  bitmaps[get_index(Color::White, PieceType::Knight)] = 0x42ULL;
-  bitmaps[get_index(Color::White, PieceType::Bishop)] = 0x24ULL;
-  bitmaps[get_index(Color::White, PieceType::Rook)] = 0x81ULL;
-  bitmaps[get_index(Color::White, PieceType::Queen)] = 0x08ULL;
-  bitmaps[get_index(Color::White, PieceType::King)] = 0x10ULL;
+//eine Art Stack muss beigelegt werden oder? do_move müsste eine Art Stack füllen mit moves und das reichen wir undo weiter.
+//ich sehe nicht wie undo jemals etwas "zurück" bringen soll wenn er nicht weiß.. doch einfach das VOR do_move einfach reinlegen?
+//heißt das ich brauche literally einen std::stack<GameState>? Wo ich pro do_move reinlege und pro undo_move es rausnehme und wiederherstelle?
 
-  bitmaps[get_index(Color::Black, PieceType::Pawn)] = 0xFF00ULL << 40;
-  bitmaps[get_index(Color::Black, PieceType::Knight)] = 0x42ULL << 56;
-  bitmaps[get_index(Color::Black, PieceType::Bishop)] = 0x24ULL << 56;
-  bitmaps[get_index(Color::Black, PieceType::Rook)] = 0x81ULL << 56;
-  bitmaps[get_index(Color::Black, PieceType::Queen)] = 0x08ULL << 56;
-  bitmaps[get_index(Color::Black, PieceType::King)] = 0x10ULL << 56;
+// void Board::undo_move(Move m) noexcept{
+
+// };
+
+Board::Board() {
+  init_bitmaps();
+  init_castling_bitmask();
 
   color_board[+Color::White] = get_color_bitmap(Color::White);
   color_board[+Color::Black] = get_color_bitmap(Color::Black);
   game_state = {Color::White, CastlingRight::Any, Square::None, 0, 1};
+
   sync_mailbox_with_bitmaps();
 }
--- src/board.hpp
+++ src/board.hpp
@@ -7,9 +7,13 @@ private:
   //Membervariablen:
   std::array<uint64_t, 14> bitmaps{};
   std::array<Piece, 64>mailbox{};
+  std::array<CastlingRight, 64> castling_mask;
   std::array<uint64_t, 2> color_board{};
+  std::array<GameState, 1024> history;
   GameState game_state{};
-
+  uint64_t ply{};
+  
+    
   //Hilfsfunktionen:
   uint8_t get_index(Color color, PieceType piece) const noexcept;
   uint64_t get_color_bitmap(Color color) const noexcept;
@@ -17,9 +21,13 @@ private:
   void add_piece(Color c, PieceType p, Square s) noexcept;
   void move_piece(Color c, PieceType p, Square from, Square to) noexcept;
   void remove_piece(Color color, PieceType piece, Square square) noexcept;
+  void init_bitmaps();
+  void init_castling_bitmask(); 
+
 public:
   Board();
-  void make_move(Move m) noexcept;
+  void do_move(Move m, GameState& gamestate) noexcept;
+  void undo_move(Move m) noexcept;
   uint64_t get_bitmap(Color color, PieceType piece) const noexcept;
   uint64_t get_color_board(Color color) const noexcept;
   const GameState& get_gamestate() const noexcept;
